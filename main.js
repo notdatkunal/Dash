9110b6ddbe9916ba9fe613262ee87c76bbec2474
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-02-16 01:22:18 +0530

    Update main.js
    js removed

--- main.js
+++ main.js
@@ -1,23 +1,2 @@
-let nav = document.getElementById("navigationBar");
-let hero = document.getElementById("heroButton");
-
-function shop(){
 
     console.log("hi");
-    popup.showModal();
-};
-
-
-function search(){
-
-    nav.style.display = 'none';
-}
-
-
-
-hero.addEventListener("click",
-function (e){
-
-    nav.style.display = 'block';
-}
-);
